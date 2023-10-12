f2a22a8922d36b2d9ac9fa41f9ca0ea5f340c6c3
MatiasPhoenix <matias.la19@gmail.com>
2023-10-12 16:59:07 +0200

    animazioni_h1_ritocchi

--- m1/s3/g4/javascript/javascript.js
+++ m1/s3/g4/javascript/javascript.js
@@ -1,40 +1,33 @@
-const tombola = document.getElementById("tombola");
 
-for (let i = 1; i < 77; i++){
-    let numero = i;
+    const tombola = document.getElementById("tombola");
+
+    for (let i = 1; i <= 76; i++) {
+        let numero = i;
+        
+        let spazioG = document.createElement("div");
+        spazioG.classList.add("numero");
+        spazioG.innerText = numero;
     
-    
-    let spazioG = document.createElement("div");
-    spazioG.classList.add("numero");
-    spazioG.innerText = numero;
-
-
-    tombola.append(spazioG);
-    
-}
-
-let currentNumber = document.querySelector("#current-number");
-let sButton = document.querySelector("#start-button");
-
-sButton.addEventListener("click", function(){
-    
-    while (currentNumber.firstChild) {
-        currentNumber.removeChild(currentNumber.firstChild);
+        tombola.appendChild(spazioG);
     }
-        newNumber = Math.floor(Math.random() * 77);
-        currentNumber.append(newNumber); 
-
-
-        let numeriUsciti = document.querySelector(".numeri");
-
-        for (let numeri of numeriUsciti){
-            if (numeri === newNumber){
-                numeriUsciti.classList.add("numeroUsciti");
-            }
-
-
+    
+    let currentNumber = document.querySelector("#current-number");
+    let sButton = document.querySelector("#start-button");
+    
+    sButton.addEventListener("click", function() {
+        while (currentNumber.firstChild) {
+            currentNumber.removeChild(currentNumber.firstChild);
         }
-
-
-    })  
-
+        
+        newNumber = Math.floor(Math.random() * 76) + 1;
+        currentNumber.append(newNumber);
+    
+        let numeriUsciti = document.querySelectorAll(".numero");
+    
+        for (let numero of numeriUsciti) {
+            if (parseInt(numero.innerText) === newNumber) {
+                numero.style.backgroundColor = "red";
+            }
+        }
+    });
+    
