d3c4e24e15060634303bee2a65c5fdf62e1ff38d
MatiasPhoenix <matias.la19@gmail.com>
2023-10-28 13:58:19 +0200

    js-css

--- m2/s1/g5/assets/javascript/script.js
+++ m2/s1/g5/assets/javascript/script.js
@@ -1,5 +1,6 @@
 
 const header = document.querySelector("header");
+const button = document.getElementsByClassName("get-started")
 let isScrolled = false; 
 let originalBackgroundColor = window.getComputedStyle(header).backgroundColor;
 
@@ -7,10 +8,15 @@ window.addEventListener("scroll", () => {
   if (window.scrollY > 400) { 
     if (!isScrolled) {
       header.style.backgroundColor = "white";
+      button.style.backgroundColor = "green";
+      
       isScrolled = true;
     }
   } else {
     header.style.backgroundColor = originalBackgroundColor; 
+    button.style.backgroundColor = "rgb(25, 25, 25)"; 
     isScrolled = false;
   }
 });
+
+
