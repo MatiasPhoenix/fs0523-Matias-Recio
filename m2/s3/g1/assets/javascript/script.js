faa588e3071cd345a8abf3ba89c2e1e60686432a
MatiasPhoenix <matias.la19@gmail.com>
2023-11-06 16:07:31 +0100

    correzione-js-primoEsercizio

--- m2/s3/g1/assets/javascript/script.js
+++ m2/s3/g1/assets/javascript/script.js
@@ -7,18 +7,21 @@ class User{
         this.location = location;
     }
 
-    confronto(user1, user2){
-        if (user1.age > user2.age){
-            return `L'user ${this.nome} è più grande.`
-        } else if (user1.age === user2.age) {
+    confrontoTraUser(user2){
+        if (this.age > user2.age){
+            return `L'user ${this.firstName} è più grande di ${user2.firstName}.`
+        } else if (this.age === user2.age) {
             return `I due user hanno la stessa età`
         } else {
-            return `L'user ${this.nome} è più grande.`
+            return `L'user ${user2.firstName} è più grande di  ${user1.firstName}.`
         }
     }
+
 }
 
-let persona1 = new User ("Pippo", "Goofy", "40", "Disney Castle")
-let persona2 = new User ("Sora", "Keyblade", "15", "Destiny Islands")
 
-console.log();
+
+let persona1 = new User ("Pippo", "Goofy", 40, "Disney Castle");
+let persona2 = new User ("Sora", "Keyblade", 15, "Destiny Islands");
+
+console.log(persona1.confrontoTraUser(persona2));
