2e6beef2aff6d774ef956c0771fed781a532e013
MatiasPhoenix <matias.la19@gmail.com>
2023-11-10 12:00:45 +0100

    progetto-settimanale

---
+++ m2/s3/progetto-settimanale/assets/javascript/script.js
@@ -0,0 +1,6 @@
+fetch("https://striveschool-api.herokuapp.com/api/put-your-endpoint-here/", {
+headers: {
+"Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTRlMGI2OTMyNWM5NzAwMTg3ZjlmODUiLCJpYXQiOjE2OTk2MTM1NDUsImV4cCI6MTcwMDgyMzE0NX0.3FuaQA2fIbbdc2B0bWxcqgFTMrWpLT-WuuMPclLOhXw"
+}
+})
+
