9f289478e2bd4045fb5a5bb9f18d3fefcae4f28a
MatiasPhoenix <matias.la19@gmail.com>
2023-11-20 16:47:58 +0100

    esercizio-aggiornamento-codice

--- m3/s1/g1/ts/script.js
+++ m3/s1/g1/ts/script.js
@@ -8,15 +8,17 @@ let player2Num = Math.ceil(Math.random() * (100 - 1) + 1);
 player2 = player2Num;
 console.log("Giocatore 2 ha scelto: " + player2);
 let numeroGioco = Math.ceil(Math.random() * (100 - 1) + 1);
-console.log(numeroGioco);
+console.log("Il numero da indovinare è " + numeroGioco);
 // let numeroGioco:number;
 function risultatoGioco(player1, player2, numeroGioco) {
-    let numeroConfronto = 100;
-    let pippo = numeroConfronto + numeroGioco;
-    if ((pippo - player1) < (pippo - player2)) {
+    let pippo = (numeroGioco + player1) - 100;
+    console.log(pippo);
+    let paperino = (numeroGioco + player2) - 100;
+    console.log(paperino);
+    if (pippo < paperino) {
         return "Il giocatore 1 ha vinto!";
     }
-    else if ((pippo - player1) > (pippo - player2)) {
+    else if (pippo > paperino) {
         return "Il giocatore 2 ha vinto!";
     }
     else {
@@ -26,3 +28,13 @@ function risultatoGioco(player1, player2, numeroGioco) {
 ;
 let vincitore = risultatoGioco(player1, player2, numeroGioco);
 console.log(vincitore);
+/*
+
+numeroMax=10
+numScelto=7
+g1=3
+g2=8
+
+10+7= 3 - g2(8) = -5.
+
+*/ 
--- m3/s1/g1/ts/script.ts
+++ m3/s1/g1/ts/script.ts
@@ -10,16 +10,18 @@ player2 = player2Num;
 console.log("Giocatore 2 ha scelto: " + player2);
 
 let numeroGioco = Math.ceil(Math.random() * (100-1)+1);
-console.log(numeroGioco);
+console.log("Il numero da indovinare è " + numeroGioco);
 // let numeroGioco:number;
 
 
 function risultatoGioco (player1:number, player2:number, numeroGioco:number):string | void {
-    let numeroConfronto:number = 100;
-    let pippo:number = numeroConfronto + numeroGioco;
-    if ((pippo - player1) < (pippo - player2)) {
+    let pippo:number =  (numeroGioco + player1) - 100;
+    console.log(pippo);    
+    let paperino:number = (numeroGioco + player2) - 100;
+    console.log(paperino);    
+    if (pippo < paperino) {
         return "Il giocatore 1 ha vinto!";
-    } else if ((pippo - player1) > (pippo - player2)) {
+    } else if (pippo > paperino) {
         return "Il giocatore 2 ha vinto!";
     } else {
         return "Capperetti! qui c'è un pareggio!";
@@ -28,3 +30,15 @@ function risultatoGioco (player1:number, player2:number, numeroGioco:number):str
 
 let vincitore:string | void = risultatoGioco(player1, player2, numeroGioco);
 console.log(vincitore);
+
+
+/*
+
+numeroMax=10
+numScelto=7
+g1=3
+g2=8
+
+10+7= 3 - g2(8) = -5.
+
+*/
