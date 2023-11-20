33c29a77efe1d8a787aacb9d7f4eee5523755219
MatiasPhoenix <matias.la19@gmail.com>
2023-11-20 15:21:27 +0100

    esercizio-m3-s1-g1

---
+++ m3/s1/g1/ts/script.js
@@ -0,0 +1,8 @@
+"use strict";
+let player1 = 0;
+let player2 = 0;
+let player1Num = Math.ceil(Math.random() * (100 - 1) + 1);
+player1 = player1Num;
+let player2Num = Math.ceil(Math.random() * (100 - 1) + 1);
+player2 = player2Num;
+let numeroConfronto = Math.ceil(Math.random() * (100 - 1) + 1);
---
+++ m3/s1/g1/ts/script.ts
@@ -0,0 +1,17 @@
+let player1:number = 0;
+let player2:number = 0;
+
+let player1Num = Math.ceil(Math.random() * (100-1)+1);
+player1 = player1Num;
+
+let player2Num = Math.ceil(Math.random() * (100-1)+1);
+player2 = player2Num;
+
+let numeroConfronto = Math.ceil(Math.random() * (100-1)+1);
+
+function risultatoGioco (player1:number, player2:number, numeroConfronto:number) {
+
+}
+
+
+// numeroConfronto - numero del giocatore
