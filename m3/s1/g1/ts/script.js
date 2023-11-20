8f8b19fd2c42e4b2df24e5d40dd2390215cfcad0
MatiasPhoenix <matias.la19@gmail.com>
2023-11-20 15:28:29 +0100

    compilato-ts

--- m3/s1/g1/ts/script.js
+++ m3/s1/g1/ts/script.js
@@ -3,6 +3,20 @@ let player1 = 0;
 let player2 = 0;
 let player1Num = Math.ceil(Math.random() * (100 - 1) + 1);
 player1 = player1Num;
+console.log("Giocatore 1 ha scelto: " + player1);
 let player2Num = Math.ceil(Math.random() * (100 - 1) + 1);
 player2 = player2Num;
+console.log("Giocatore 2 ha scelto: " + player2);
 let numeroConfronto = Math.ceil(Math.random() * (100 - 1) + 1);
+function risultatoGioco(player1, player2, numeroConfronto) {
+    if ((numeroConfronto - player1) > (numeroConfronto - player2)) {
+        console.log("Il giocatore 2 ha vinto!");
+    }
+    else if ((numeroConfronto - player1) < (numeroConfronto - player2)) {
+        console.log("Il giocatore 1 ha vinto!");
+    }
+    else {
+        console.log("Capperetti! qui c'è un pareggio!");
+    }
+}
+;
--- m3/s1/g1/ts/script.ts
+++ m3/s1/g1/ts/script.ts
@@ -3,15 +3,20 @@ let player2:number = 0;
 
 let player1Num = Math.ceil(Math.random() * (100-1)+1);
 player1 = player1Num;
+console.log("Giocatore 1 ha scelto: " + player1);
 
 let player2Num = Math.ceil(Math.random() * (100-1)+1);
 player2 = player2Num;
+console.log("Giocatore 2 ha scelto: " + player2);
 
 let numeroConfronto = Math.ceil(Math.random() * (100-1)+1);
 
 function risultatoGioco (player1:number, player2:number, numeroConfronto:number) {
-
-}
-
-
-// numeroConfronto - numero del giocatore
+    if ((numeroConfronto - player1) > (numeroConfronto - player2)) {
+        console.log("Il giocatore 2 ha vinto!");
+    } else if ((numeroConfronto - player1) < (numeroConfronto - player2)) {
+        console.log("Il giocatore 1 ha vinto!");
+    } else {
+        console.log("Capperetti! qui c'è un pareggio!");
+    }
+};
