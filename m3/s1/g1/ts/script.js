3bbc5dadb7259b2f3d06ce194d50c3fef73cb487
MatiasPhoenix <matias.la19@gmail.com>
2023-11-20 16:08:33 +0100

    esercizio-completo-(risultatoSbaglito)

--- m3/s1/g1/ts/script.js
+++ m3/s1/g1/ts/script.js
@@ -7,16 +7,22 @@ console.log("Giocatore 1 ha scelto: " + player1);
 let player2Num = Math.ceil(Math.random() * (100 - 1) + 1);
 player2 = player2Num;
 console.log("Giocatore 2 ha scelto: " + player2);
-let numeroConfronto = Math.ceil(Math.random() * (100 - 1) + 1);
-function risultatoGioco(player1, player2, numeroConfronto) {
-    if ((numeroConfronto - player1) > (numeroConfronto - player2)) {
-        console.log("Il giocatore 2 ha vinto!");
+let numeroGioco = Math.ceil(Math.random() * (100 - 1) + 1);
+console.log(numeroGioco);
+// let numeroGioco:number;
+function risultatoGioco(player1, player2, numeroGioco) {
+    let numeroConfronto = 100;
+    let pippo = numeroConfronto + numeroGioco;
+    if ((pippo - player1) < (pippo - player2)) {
+        return "Il giocatore 1 ha vinto!";
     }
-    else if ((numeroConfronto - player1) < (numeroConfronto - player2)) {
-        console.log("Il giocatore 1 ha vinto!");
+    else if ((pippo - player1) > (pippo - player2)) {
+        return "Il giocatore 2 ha vinto!";
     }
     else {
-        console.log("Capperetti! qui c'è un pareggio!");
+        return "Capperetti! qui c'è un pareggio!";
     }
 }
 ;
+let vincitore = risultatoGioco(player1, player2, numeroGioco);
+console.log(vincitore);
--- m3/s1/g1/ts/script.ts
+++ m3/s1/g1/ts/script.ts
@@ -9,14 +9,22 @@ let player2Num = Math.ceil(Math.random() * (100-1)+1);
 player2 = player2Num;
 console.log("Giocatore 2 ha scelto: " + player2);
 
-let numeroConfronto = Math.ceil(Math.random() * (100-1)+1);
+let numeroGioco = Math.ceil(Math.random() * (100-1)+1);
+console.log(numeroGioco);
+// let numeroGioco:number;
 
-function risultatoGioco (player1:number, player2:number, numeroConfronto:number) {
-    if ((numeroConfronto - player1) > (numeroConfronto - player2)) {
-        console.log("Il giocatore 2 ha vinto!");
-    } else if ((numeroConfronto - player1) < (numeroConfronto - player2)) {
-        console.log("Il giocatore 1 ha vinto!");
+
+function risultatoGioco (player1:number, player2:number, numeroGioco:number):string | void {
+    let numeroConfronto:number = 100;
+    let pippo:number = numeroConfronto + numeroGioco;
+    if ((pippo - player1) < (pippo - player2)) {
+        return "Il giocatore 1 ha vinto!";
+    } else if ((pippo - player1) > (pippo - player2)) {
+        return "Il giocatore 2 ha vinto!";
     } else {
-        console.log("Capperetti! qui c'è un pareggio!");
+        return "Capperetti! qui c'è un pareggio!";
     }
 };
+
+let vincitore:string | void = risultatoGioco(player1, player2, numeroGioco);
+console.log(vincitore);
