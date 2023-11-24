b70ae790c7e008fa54f24255492252e7219bc6f8
MatiasPhoenix <matias.la19@gmail.com>
2023-11-24 11:10:29 +0100

    interface

--- m3/s1/ProgettoSettimanale/script.ts
+++ m3/s1/ProgettoSettimanale/script.ts
@@ -1,1 +1,22 @@
-interface
+interface Sim {
+    carica:number, //carica (soldi) della sim
+    numeroChiamate:number, //quante chiamate ha ricevuto la sim
+    costoMinuto:number //il costo, al minuto, quando si effettuano chiamate
+
+    //metodi del interface
+    ricarica(euro:number):void;
+    
+    numero404():string;
+
+    getNumeroChiamate():number;
+
+    chiamata(min:number):void;
+
+    azzeraChiamate():void;
+
+}
+
+
+
+
+
