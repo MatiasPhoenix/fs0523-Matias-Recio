73a1c7b2b09cd200ac40eacb93d79437ed0fcf49
MatiasPhoenix <matias.la19@gmail.com>
2023-11-28 16:43:55 +0100

    commit

--- m3/s2/g2/esercizio/src/app/new-service.service.ts
+++ m3/s2/g2/esercizio/src/app/new-service.service.ts
@@ -5,5 +5,51 @@ import { Injectable } from '@angular/core';
 })
 export class NewServiceService {
 
-  constructor() { }
+  service:IService[] = [
+    {
+      "id":1,
+      "body": "bla bla bla bla bla bla bla bla bla",
+      "title": "TITOLONE",
+      "active": true
+    },
+    {
+      "id":2,
+      "body": "bla bla bla bla bla bla bla bla bla",
+      "title": "TITOLONE",
+      "active": true
+    },
+    {
+      "id":3,
+      "body": "bla bla bla bla bla bla bla bla bla",
+      "title": "TITOLONE",
+      "active": true
+    },
+    {
+      "id":4,
+      "body": "bla bla bla bla bla bla bla bla bla",
+      "title": "TITOLONE",
+      "active": true
+    },
+    {
+      "id":5,
+      "body": "bla bla bla bla bla bla bla bla bla",
+      "title": "TITOLONE",
+      "active": false
+    },
+    {
+      "id":6,
+      "body": "bla bla bla bla bla bla bla bla bla",
+      "title": "TITOLONE",
+      "active": false
+    },
+
+  ];
+
+  getActivePost(){
+    return this.service.filter(p => p.active)
+  }
+
+  getInactivePost(){
+    return this.service.filter(p => !p.active)
+  }
 }
