fbac58ce11854c2f36e36ca78685d7ae4fa34a19
MatiasPhoenix <matias.la19@gmail.com>
2023-11-30 15:22:13 +0100

    array-service

--- m3/s2/g4/esercizio/src/app/app-routing.module.ts
+++ m3/s2/g4/esercizio/src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { InattiviComponent } from './pages/inattivi/inattivi.component';
+import { AttiviComponent } from './pages/attivi/attivi.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'Attivi',
+    component: AttiviComponent
+  },
+  {
+    path: 'Inattivi',
+    component: InattiviComponent
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- m3/s2/g4/esercizio/src/app/page-posts.service.ts
+++ m3/s2/g4/esercizio/src/app/page-posts.service.ts
@@ -1,9 +1,65 @@
 import { Injectable } from '@angular/core';
+import { IPosts } from './Models/iposts';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PagePostsService {
 
+  posts:IPosts[] = [
+    {
+      id: 0,
+      body:"bla bla bla bla bla bla bla bla bla bla",
+      title: "Super Titolo news0",
+      active: true,
+      type: "news"
+    },
+    {
+      id: 1,
+      body:"bla bla bla bla bla bla bla bla bla bla",
+      title: "Super Titolo politic1",
+      active: true,
+      type: "politic"
+    },
+    {
+      id: 2,
+      body:"bla bla bla bla bla bla bla bla bla bla",
+      title: "Super Titolo news2",
+      active: false,
+      type: "news"
+    },
+    {
+      id: 3,
+      body:"bla bla bla bla bla bla bla bla bla bla",
+      title: "Super Titolo news3",
+      active: true,
+      type: "news"
+    },
+    {
+      id: 4,
+      body:"bla bla bla bla bla bla bla bla bla bla",
+      title: "Super Titolo politic4",
+      active: false,
+      type: "politic"
+    },
+    {
+      id: 5,
+      body:"bla bla bla bla bla bla bla bla bla bla",
+      title: "Super Titolo news5",
+      active: true,
+      type: "news"
+    },
+    {
+      id: 6,
+      body:"bla bla bla bla bla bla bla bla bla bla",
+      title: "Super Titolo news6",
+      active: true,
+      type: "news"
+    }
+  ];
+
   constructor() { }
+
+
+
 }
