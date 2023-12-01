75e01b15f36261f78d9d1486460dc90971d5aa40
MatiasPhoenix <matias.la19@gmail.com>
2023-12-01 10:42:30 +0100

    progetto-settimanale

---
+++ m3/s2/ProgettoSettimanale/todoList/src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { TodoComponent } from './Pages/todo/todo.component';
+import { CompletedComponent } from './Pages/completed/completed.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: TodoComponent
+  },
+  {
+    path: 'completed',
+    component: CompletedComponent
+  },
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
